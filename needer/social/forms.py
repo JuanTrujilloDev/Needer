b14d278eaf40223730f9fc4bd54d81a480a03ffb
Juan Trujillo <juantrujillo.dev@gmail.com>
2022-10-09 17:15:42 -0500

    Correccion en formulario Publicacion y cantidad de caracteres

--- needer/social/forms.py
+++ needer/social/forms.py
@@ -5,7 +5,7 @@ from tinymce import widgets as tinymce_widgets
 
 class CrearPublicacionForm(forms.ModelForm):
     
-    descripcion = forms.CharField(max_length=280, widget=tinymce_widgets.AdminTinyMCE(attrs={'placeholder':'Cuentale a tu publico de ti... (Maximo 280 caracteres)',}))
+    descripcion = forms.CharField(required=False, widget=tinymce_widgets.AdminTinyMCE(attrs={'placeholder':'Cuentale a tu publico de ti... (Maximo 280 caracteres)',}))
     class Meta:
         model = Publicacion
         fields = ['descripcion', 'archivo', 'nsfw']
@@ -27,13 +27,18 @@ class CrearPublicacionForm(forms.ModelForm):
 
     def clean(self):
         cleaned_data =  super().clean()
-
-        if len(cleaned_data['descripcion']) > 280:
-            raise forms.ValidationError('La descripcon debe tener menos de 280 caracteres')
+        print(cleaned_data)
+        if not 'descripcion' in cleaned_data:
+            cleaned_data['descripcion'] = ''
+        else:
+            if len(cleaned_data['descripcion']) > 2200:
+                raise forms.ValidationError('La descripcon debe tener menos de 280 caracteres')
 
         # Si ambos campos van vacios tirara error.
         if len(cleaned_data['descripcion']) == 0 and not cleaned_data["archivo"]:
             raise forms.ValidationError('Debes agregar una descripcion o un archivo a la publicacion.')
+
+        return cleaned_data
 
 
 class CrearComentarios(forms.ModelForm):
