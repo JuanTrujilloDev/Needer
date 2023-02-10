d665c7677636b0fbb14a0c77bfa24f91aea8841e
Juan David Trujillo <57489455+JuanTrujilloDev@users.noreply.github.com>
2023-02-10 00:53:39 -0500

    1. Se modifico las vistas del chat
    2. Se realizaron los cambios del frontend.
    
    TO DO: Corregir routing y realizar el backend de los mensajes

--- needer/chat/views.py
+++ needer/chat/views.py
@@ -3,9 +3,11 @@ from django.shortcuts import render
 from django.views.generic import ListView, DetailView
 from django.contrib.auth.mixins import LoginRequiredMixin
 from main.utils import ExtendsInnerContentMixin
+from django.shortcuts import redirect
+from django.urls import reverse
 
 # Create your views here.
-from chat.models import Thread
+from chat.models import Thread, ChatMessage
 
 
 
@@ -19,7 +21,38 @@ class ThreadListView (ExtendsInnerContentMixin, LoginRequiredMixin, ListView):
         return Thread.objects.by_user(user= self.request.user).order_by('-timestamp')
 
 
+# TODO meterle seguridad a los chats
 class ThreadDetailView (ExtendsInnerContentMixin, LoginRequiredMixin, DetailView):
 
     model = Thread
     template_name = 'chat/thread.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['me'] = self.request.user
+        context['thread'] = Thread.objects.get(id = self.kwargs['pk'])
+
+        if self.request.user ==  context['thread'].first_person:
+            context['user'] =  context['thread'].second_person
+
+        else:
+            context['user'] =  context['thread'].first_person
+
+        context['messages'] = ChatMessage.objects.filter(thread= context['thread'])
+
+        return context
+    
+
+    def get(self, request, **kwargs):
+        thread = Thread.objects.get(id = self.kwargs['pk'])
+
+        print(thread.second_person.username)
+        if self.request.user ==  thread.first_person or self.request.user ==  thread.second_person:
+
+            return render(request, self.template_name, kwargs)
+
+        else:
+            return redirect(reverse('chat'))
+
+
+    #POST AJAX 
