a9b3fc546564820139584653e45abdc10436fcca
Juan David Trujillo <57489455+JuanTrujilloDev@users.noreply.github.com>
2023-02-16 17:17:25 -0500

    Landing Page, About, Terminos y Politicas

--- needer/main/urls.py
+++ needer/main/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path, include
-from .views import HomeView
+from .views import HomeView, AboutView, TerminosView, PoliticasView
 
 urlpatterns = [
     
 path('', HomeView.as_view(), name="home-view"),
+path('about/', AboutView.as_view(), name="about-view"),
+path('terminos/', TerminosView.as_view(), name="terminos-view"),
+path('politicas/', PoliticasView.as_view(), name="politicas-view"),
 
 ]
--- needer/main/views.py
+++ needer/main/views.py
@@ -21,3 +21,59 @@ class HomeView (TemplateView):
             return redirect(reverse('social-home'))
 
         return super().get(request, *args, **kwargs)
+
+
+class AboutView (TemplateView):
+    template_name = 'main/about.html'
+
+    def get_context_data(self, **kwargs):
+
+        context = super().get_context_data(**kwargs)
+        context['title'] = 'Needer | About'
+
+        return context
+
+
+
+    def get(self, request, *args, **kwargs):
+        if self.request.user.is_authenticated:
+            return redirect(reverse('social-home'))
+
+        return super().get(request, *args, **kwargs)
+
+class PoliticasView (TemplateView):
+    template_name = 'main/politicas.html'
+
+    def get_context_data(self, **kwargs):
+
+        context = super().get_context_data(**kwargs)
+        context['title'] = 'Needer | Politicas de Privacidad'
+
+        return context
+
+
+
+    def get(self, request, *args, **kwargs):
+        if self.request.user.is_authenticated:
+            return redirect(reverse('social-home'))
+
+        return super().get(request, *args, **kwargs)
+
+
+class TerminosView (TemplateView):
+    template_name = 'main/terminos.html'
+
+    def get_context_data(self, **kwargs):
+
+        context = super().get_context_data(**kwargs)
+        context['title'] = 'Needer | Terminos y Condiciones'
+
+        return context
+
+
+
+    def get(self, request, *args, **kwargs):
+        if self.request.user.is_authenticated:
+            return redirect(reverse('social-home'))
+
+        return super().get(request, *args, **kwargs)
